9e23fea44f684f5378ddde189daf31c82ac845f8
Yury-96 <fiz12@bk.ru>
2020-10-20 11:19:55 +0300

    'Reliz-0'

---
+++ get_absolute_url.py
@@ -0,0 +1,14 @@
+def get_absolute_url(url, *args, **kwargs):
+    s = ''
+    s = s + url
+    for i in args:
+        s = s + '/' + i
+    s = s + '?'
+    for k, v in kwargs.items():
+        s = s + k + '=' + v + '&'
+    s = s[:len(s)-1]
+    return s
+
+
+print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
+print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
