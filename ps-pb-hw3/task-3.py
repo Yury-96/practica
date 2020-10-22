be73129473c7713407ae4239c2ddbd145137cb48
Yury-96 <fiz12@bk.ru>
2020-10-22 11:50:59 +0300

    'Reliz-1'

--- ps-pb-hw3/task-3.py
+++ ps-pb-hw3/task-3.py
@@ -3,15 +3,16 @@ summ = 0
 previous = 0
 current = 1
 while current<10**7:
+    if current % 2 == 0:
+        print(current, end='  ')
+        summ +=current
     quantity +=1
-    print(current, end='  ')
     temp = current
     current +=previous
     previous = temp
-    if current % 2 == 0 and current<10**7:
-        summ +=current
-        #print(current, end='  ')
+
 print()
 print(f'Количество элементов последовательности: {quantity}')
 print(f'Сумма всех чётных элементов последовательности: {summ}')
 print(f'Предпоследнее число заданной последовательности: {previous}')
+# Последним считаем число, которое уже не меньше 10000000
